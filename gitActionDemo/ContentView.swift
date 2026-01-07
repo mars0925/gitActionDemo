fbbf670db173b81ba3f51a19b23680fd8384d217
mars0925 <mars09250148@gmail.com>
2026-01-07 14:36:55 +0800

    Initial Commit

---
+++ gitActionDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gitActionDemo
+//
+//  Created by MarsChang on 2026/1/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gitActionDemo/gitActionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitActionDemoApp.swift
+//  gitActionDemo
+//
+//  Created by MarsChang on 2026/1/7.
+//
+
+import SwiftUI
+
+@main
+struct gitActionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
